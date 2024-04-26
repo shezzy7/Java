5f904f7adc01501584e5d7eef396aff253941059
shezzy7 <shahzadhussain72242@gmail.com>
2024-04-26 11:42:17 +0500

    java

--- Day.13(Bit Manipulation)/clear_LastBits.java
+++ Day.13(Bit Manipulation)/clear_LastBits.java
@@ -1,12 +1,5 @@
 public class clear_LastBits{
     public static void main(String args[]){
-        int n = 15;
-        //number of bits to clear at last
-        int bit = 2;
-        //if take ~(0) then it becomes 11111111 and then if we get its bitMask by bit number as here bit number is 2 so 11111111<<2=11111100 and then add then perform AND operation between given value and this bitmask we get the answer as 00001111 AND 11111100 = 00001100=12.
-        int x = ~(0);
-        int bitMask=x<<bit;
-        n=n&bitMask;
-        System.out.print("Value after clearing last "+bit+" bits = "+n);
+        
     }
 }
---
+++ Day.14(OOP)/b_access_specifiers.java
@@ -0,0 +1,25 @@
+public class b_access_specifiers{
+    public static void main(String args[]){
+
+    }
+}
+
+class Bank{
+    //access specifiers defines the accesibility of attributes and functions of a class outside that class
+    //There are four types of access specifiers.
+    // Access Modifer | within class | within package | 
+    // ------------------------------------------------
+    // Private        | Yes          | No             | 
+    // Default        | Yes          | Yes            |
+    // Protected      | Yes          | Yes            |
+    // public         | Yes          | Yes            |
+    
+    // Access Modifer | within package by subclass only | ouside package | 
+    // -------------------------------------------------------------------
+    // Private        | No                              | No             | 
+    // Default        | No                              | Yes            |
+    // Protected      | Yes                             | No             |
+    // public         | Yes                             | Yes            |
+    
+
+}
