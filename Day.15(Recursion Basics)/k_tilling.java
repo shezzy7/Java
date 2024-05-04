dada6f1b1586594d45c85aeceed8cd1a72d6c607
shezzy7 <shahzadhussain72242@gmail.com>
2024-05-04 18:43:23 +0500

    java

--- Day.15(Recursion Basics)/k_tilling.java
+++ Day.15(Recursion Basics)/k_tilling.java
@@ -10,7 +10,9 @@ public class k_tilling{
         return fnm1+fnm2;
     }
     public static void main(String args[]){
+        //we are given width of floor(n).Height of floor is 2.And we have to find number of ways to palce tiles in this floor.Each tile is of 2*1(height,width). 
         int n=5;
+        
         System.out.print(ways(n));
     }
 }
