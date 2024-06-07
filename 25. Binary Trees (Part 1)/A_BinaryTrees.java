52317fd07ef2a67fc50c490643f5b327e2408c0d
shezzy7 <shahzadhussain72242@gmail.com>
2024-06-07 18:07:11 +0500

    java

---
+++ 25. Binary Trees (Part 1)/A_BinaryTrees.java
@@ -0,0 +1,6 @@
+
+public class A_BinaryTrees{
+
+    //Tree is hierarchial data structure.In this data strucutre elements are stored like a family chain.
+    //Binary Tree is a specific type of tree in which each node can contains maximum 2 nodes(childrens)
+}
