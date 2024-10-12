7cfcab4932ff0477f1b09d73f3a0921d75774bfc
shezzy7 <jamshahzad72242@gmail.com>
2024-10-12 19:11:36 +0500

    Java

---
+++ 25.Binary_Trees(Part1)/B_createTreeUsingPreorder.java
@@ -0,0 +1,43 @@
+public class B_createTreeUsingPreorder {
+    public static class Node {
+        int data;
+        Node right;
+        Node left;
+
+        public Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    // preorder->Pre-order means first we create a root node then its left child and then its right child.(root node,left node,right node)
+    static class BinaryTree {
+        static int idx = -1;
+
+        public static Node buildTree(int nodes[]) {
+            idx++;
+            if (nodes[idx] == -1) {//-1 represents a null node
+                return null;
+            }
+            // kaam
+            Node newNode = new Node(nodes[idx]);//root node
+            newNode.left = buildTree(nodes);//left child
+            newNode.right = buildTree(nodes);//right child
+
+            return newNode;
+
+        }
+
+        
+    }
+
+    public static void main(String args[]) {
+        // Building a tree with preorder method
+        int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
+        BinaryTree tree = new BinaryTree();
+        Node root = tree.buildTree(nodes);
+        System.out.print(root.data + " ");
+
+    }
+}
---
+++ 25.Binary_Trees(Part1)/F_levelOrderTraversal.java
@@ -0,0 +1,62 @@
+import java.util.*;
+public class F_levelOrderTraversal {
+     static class Node{
+        int data;
+        Node left,right;
+        public Node(int data){
+            this.data=data;
+            this.left=null;
+            this.right=null;
+        }
+    }
+     static class BinaryTree{
+
+        static int idx=-1;
+        public static Node buildTree(int nodes[]){
+            idx++;
+            if(nodes[idx]==-1){
+                return null;
+            }
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
+            return newNode;
+        }
+    
+    }
+    public static void levelOrderTrav(Node root){
+        //In level order traversal we print all the elements level wise.Mean elements present at same level would be printed then we will move to next level and will print all the elements present at that level.
+        //For print our elements in level order we can use a queue.In this queue will add elements present at same level and then as we will go to print a element we will add its childs(left,right) in queue.And when we print other element we will all of its childs to queue.
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        q.add(null);
+        while(!q.isEmpty() && q.peek()!=null){
+            if(q.peek().left!=null){
+
+                q.add(q.peek().left);
+            }
+            if(q.peek().right!=null){
+
+                q.add(q.peek().right);
+            }
+            
+            System.out.print(q.remove().data);
+            if(q.peek()==null){
+                q.remove();
+                System.out.println();
+                if(!q.isEmpty()){
+
+                    q.add(null);
+                }
+            }
+        }
+        
+    }
+    public static void main(String args[]){
+        int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+        BinaryTree tree = new BinaryTree();
+        Node root = tree.buildTree(nodes);
+        levelOrderTrav(root);
+
+    }
+}
