c4cce7ad47fa712ff5d173e81e3fca709ea4cb30
shezzy7 <jamshahzad72242@gmail.com>
2024-11-01 08:56:53 +0500

    Java

---
+++ Java/29.Binary_Search_Trees(Part2)/A_sortedArrayToBST.java
@@ -0,0 +1,8 @@
+public class A_sortedArrayToBST{
+
+    public static void main(String args[]){
+        int values[] = {3,4,5,6,7,8,9,10,11,12};
+        // We are given a sorted array of integers.We have to construct a BST from it , with minimum possible depth.
+        // We can make BST from this array in this way that our root node is ,its right is 4,its right is 5 and son in this sequence , such that each next node is right child of current node , in sequence of given array and it will be a BST but its depth will be high.And we have to make such a BST , whose depth should be minimum as much as possible.
+    }
+}
