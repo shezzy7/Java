95b5f6ce6bf80f2a62eee6a39a97eff765211393
shezzy7 <jamshahzad72242@gmail.com>
2024-11-01 09:24:26 +0500

    Java

--- Java/29.Binary_Search_Trees(Part2)/A_sortedArrayToBST.java
+++ Java/29.Binary_Search_Trees(Part2)/A_sortedArrayToBST.java
@@ -1,3 +1,6 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class A_sortedArrayToBST {
     static class Node {
         int val;
@@ -26,9 +29,35 @@ public class A_sortedArrayToBST {
                                                    // greater than our mid index's value
         return root;
     }
+     public static void levelOrder(Node root){
 
+        Queue<Node>q = new LinkedList<>();
+        q.add(root);
+        q.add(null);
+        while(!q.isEmpty()){
+            Node curr = q.remove();
+
+            if(curr==null){
+                System.out.println();
+                if(q.isEmpty()){
+                    break;
+                }
+                q.add(null);
+                continue;
+            }
+            System.out.print(curr.val+" ");
+            if(curr.left!=null){
+                q.add(curr.left);
+            }
+            if(curr.right!=null){
+                q.add(curr.right);
+            }
+
+
+        }
+     }
     public static void main(String args[]) {
-        int values[] = { 3, 5, 6, 8, 10, 11, 12, 14 };
+        int values[] = { 3, 5, 6, 8, 10, 11 , 12 };
         // We are given a sorted array of integers.We have to construct a BST from it ,
         // with minimum possible depth.
         // We can make BST from this array in this way that our root node is ,its right
@@ -44,7 +73,17 @@ public class A_sortedArrayToBST {
         // nd right subtree , then in this way we can contrcut a BST with possible
         // minimum depth.
         Node root = buildBST(values, 0, values.length - 1);// for calculating mid we will pass starting and ending index of array 
-        
+
+        //output
+        /*
+                        8
+                      /   \
+                     5      11
+                   /  \     / \
+                  3    6   10  12
+         */ 
+        //lets print level order traversal of this to indentify it.
+        levelOrder(root);
 
     }
 }
