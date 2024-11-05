755a3a01a593a420e8566371ff5eeabdd1067ce3
shezzy7 <jamshahzad72242@gmail.com>
2024-11-05 18:15:01 +0500

    Java

--- Java/29.Binary_Search_Trees(Part2)/D_maxSumBST.java
+++ Java/29.Binary_Search_Trees(Part2)/D_maxSumBST.java
@@ -10,39 +10,38 @@ public class D_maxSumBST {
     }
     static class Info{//creating a class which will help in containg info of each node
         boolean isValid;
-        int size;
+        int sum;
         int max,min;
-        public Info(boolean isValid , int size , int min , int max){
+        public Info(boolean isValid , int sum , int min , int max){
             this.isValid = isValid ;
-            this.size = size;
+            this.sum = sum;
             this.min = min;
             this.max = max;
         }
     }
-    public static Node maxNode = null;//store the node of maxmum BST in this
-
-    public static Info sizeOfLargestBST(Node root){
+    public static int res = 0;
+    public static Info sumOfLargestBST(Node root){
         //first of all we will find the node which makes our BST with maximum size
         if(root==null){//if root is null then it means that it is a BST with size of 0 and we will pass +infinity as minimum value from left subtree and -infinity as maximum value from right subtree,so that on calculating min value for other nodes their min and max value will be updated as min will be comparing itself with +infinity and max with -infinity 
             return new Info(true , 0 , Integer.MAX_VALUE ,  Integer.MIN_VALUE );
         }
-        Info leftInfo = sizeOfLargestBST(root.left);//cal info of left subtree and store it in a object
-        Info rightInfo = sizeOfLargestBST(root.right);//cal info of right subtree and store it in a object
-        int size = leftInfo.size + rightInfo.size + 1; //size of current node will be equall to size of left subtree and right subtree plus its own size
+        Info leftInfo = sumOfLargestBST(root.left);//cal info of left subtree and store it in a object
+        Info rightInfo =sumOfLargestBST(root.right);//cal info of right subtree and store it in a object
+        int curSum = leftInfo.sum + rightInfo.sum + root.val; //size of current node will be equall to size of left subtree and right subtree plus its own size
         //Note : here size means total number nodes not height or depth 
         int min = Math.min(root.val , Math.min(leftInfo.min , rightInfo.min));//get min value for root node
         int max = Math.max(root.val , Math.max(rightInfo.max , leftInfo.max));//get max value
 
-        if(root.val <= leftInfo.max || root.val>=rightInfo.min){//compare if root node not making a BST
-            return new Info(false,size,min,max);
+        if(root.val < leftInfo.max || root.val>rightInfo.min){//compare if root node not making a BST
+            return new Info(false,0,min,max);
         }
 
         if(leftInfo.isValid && rightInfo.isValid){//if root node is also a BST
-            maxNode = root;
-            return new Info(true, size, min, max);
+            res = Math.max(res,curSum);
+            return new Info(true, curSum, min, max);
         }
 
-        return new Info(false,size,min ,max);//else return false
+        return new Info(false,0,min ,max);//else return false
     }
 
     public static int calSum(Node root){//now traverse overt this tree and calculte sum of all its nodes
@@ -64,11 +63,11 @@ public class D_maxSumBST {
                               65    63
         */ 
 
-        Node root = new Node(-4);
-        root.left = new Node(-2);
-        // root.left.left = new Node(5);
-        // root.left.right = new Node(20);
-        root.right = new Node(-5);
+        Node root = new Node(50);
+        root.left = new Node(30);
+        root.left.left = new Node(5);
+        root.left.right = new Node(20);
+        root.right = new Node(60);
         // root.right.left = new Node(45);
         // root.right.right = new Node(70);
         // root.right.right.left = new Node(65);
@@ -76,9 +75,9 @@ public class D_maxSumBST {
 
         //We are given a BT and we are asked to find the sum of all the nodes of largest BST in this BT.
 
-        calInfo(root);
-        int sum = calSum(maxNode);
-        System.out.println("Sum of all the nodes of largest Binary Search Tree is : "+sum);
+        sumOfLargestBST(root);
+        
+        System.out.println("Sum of all the nodes of largest Binary Search Tree is : "+res);
 
 
     }
