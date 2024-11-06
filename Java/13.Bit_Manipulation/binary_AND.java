74e3999a856e38a78458532bd6e755f697c01a55
shezzy7 <jamshahzad72242@gmail.com>
2024-11-06 18:20:31 +0500

    Java

--- Java/13.Bit_Manipulation/binary_AND.java
+++ Java/13.Bit_Manipulation/binary_AND.java
@@ -1,6 +1,6 @@
 public class binary_AND{
     public static void main(String args[]){
-        //AND(&) is used to get result after adding given values mean if we it two values 5 and 6 i will first convert them to binary numbers and then will peromrm anding operation them according to which 
+        //AND(&) is used to get result after adding given values mean if we add two values 5 and 6 using & then it will first convert them to binary numbers and then will perform anding operation upon them according to which 
         //  0 & 0 = 0 
         //  0 & 1 = 0 
         //  1 & 0 = 0 
@@ -11,7 +11,7 @@ public class binary_AND{
         //on adding these two value by & operator we'll get 100 which is equall to 4.
         //lets print it
         int res = 5&6;
-        //keep in mind bitwise operator will be written one time only mean i we write && htne shows comparison operation .So we will use only &
+        //keep in mind bitwise operator will be written one time only. keep in mind &&  is a comparison operation. So we will use only &
         System.out.println(res);
         int x = 5;
         int y = 15;
