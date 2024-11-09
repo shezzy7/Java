e94e68f94bdda8f9b8f4655d910e66cc1ca44409
shezzy7 <jamshahzad72242@gmail.com>
2024-11-09 18:58:29 +0500

    Java

--- Java/13.Bit_Manipulation/swap_values.java
+++ Java/13.Bit_Manipulation/swap_values.java
@@ -1,8 +1,8 @@
 public class swap_values{
     public static void main(String args[]){
         //With the help of bit manipulation we can swap two values with each other,without using any third variable with the help of XOR ^ operator
-        int x = 7;
-        int y = 20;
+        int x = 7;//    00111
+        int y = 20;//   10100
         System.out.println("Values before \n x = "+x+" and y = "+y); 
         x=x^y;
         y=x^y;
