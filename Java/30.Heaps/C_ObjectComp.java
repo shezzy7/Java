c4d8ace2632fa362c0697f6b93a1019230303441
shezzy7 <jamshahzad72242@gmail.com>
2024-11-11 23:49:27 +0500

    Java

---
+++ Java/30.Heaps/C_ObjectComp.java
@@ -0,0 +1,6 @@
+public class C_ObjectComp {
+    
+    public static void main(String[] args) {
+        //What if we are asked to add objects of a class in PQ , then how we will add them in our PQ.Mean how we will compare one object with other to set it priority for adding it into PQ.
+    }
+}
