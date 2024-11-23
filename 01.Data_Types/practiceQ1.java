d915714e18bb0d7a3dea3aa4f6182ec2851a4e4d
Shahzad Hussain <shahzadhussain72242@gmail.com>
2024-11-23 22:56:23 +0500

    Delete 01.Data_Types directory

--- 01.Data_Types/practiceQ1.java
+++
@@ -1,5 +0,0 @@
-public class practiceQ1 {
-    public static void main(String arg[]){
-        System.out.print("****\n***\n**\n*");
-    }
-}
--- 01.Data_Types/sum.java
+++
@@ -1,8 +0,0 @@
-public class sum {
-    public static void main(String arg[]){
-        int a = 5;
-        int b = 10;
-        int z = a+b;
-        System.out.print(z);
-    }
-}
