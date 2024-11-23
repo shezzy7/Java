e40183bd4c6b99f19273120f637f4aeeac564901
Shahzad Hussain <shahzadhussain72242@gmail.com>
2024-11-23 22:56:57 +0500

    Delete 02.Operators directory

--- 02.Operators/practiceQ1.java
+++
@@ -1,9 +0,0 @@
-public class practiceQ1 {
-    public static void main(String arg[]){
-        int x =2, y =5;
-        int exp1 = (x* y / x);
-        int exp2 = (x * (y/x));
-        System.out.print(exp1 + "," + exp2);
-
-    }
-}
--- 02.Operators/practiceQ4.java
+++
@@ -1,9 +0,0 @@
-public class practiceQ4 {
-    public static void main(String arg[]){
-        int x = 9, y=12;
-        int a=2,b=4,c=6;
-        int emp = 4/3*(x+34)+9*(a+b*c)+(3+y*(2+a))/(a+b*y);
-        System.out.print(emp);
-
-    }
-}
