b76375fc554718dc3ff77a6d916bc6affa202710
Shahzad Hussain <shahzadhussain72242@gmail.com>
2024-11-23 22:57:54 +0500

    Delete 04.Loops directory

--- 04.Loops/forLoop.java
+++
@@ -1,9 +0,0 @@
-public class forLoop {
-    public static void main(String arg[]){
-        //syntax:- for(initialization;condition;updation){body}
-        //print shezzy 10 times
-        for(int i=0;i<10;i++){
-            System.out.println("Shezzy ");
-        }
-    }
-}
--- 04.Loops/practiceQ1.java
+++
@@ -1,9 +0,0 @@
-public class practiceQ1 {
-    public static void main(String arg[]){
-        int n = 1;
-        while(n<11){
-            System.out.println(n);
-            n++;
-        }
-    }
-}
