b57256293b304de045431d4e932a6199a19537ab
shezzy7 <jamshahzad72242@gmail.com>
2025-01-13 19:27:59 +0500

    Java

--- Java/33.Graphs_part1/A_graph.java
+++ Java/33.Graphs_part1/A_graph.java
@@ -1,5 +1,13 @@
 public class A_graph {
     
-    // Graph:-A graph is a network of nodes , in which multiple node are connected tohether with help of edges.Each node in graph is called veritx, and each connecting line is called edge.
-    // Graph is implemented in many application like google maps etc
+    /*  Graph:-A graph is a network of nodes , in which multiple node are connected tohether with help of edges.Each node in graph is called vertex, and each connecting line connecting a vertex to other is called edge.
+     Graph is implemented in many application like google maps etc.
+
+      Types od edges:-
+        Edges can be Uni-directional Or Bi-directional.Uni directional mean we can go from one vertix to other but can't from second to first vertix.In Bi-directional edges we can to both vertices from any vertix.Uni directional are like one way road , while bi-directional are like two way road.We also call bi-directional as un-directed edges.
+
+        Each edge can contain some weight(+ve/-ve).Graphs those edges contain some weight are called weighted graphs and others are called un-weighted graphs.Weight can be positive or negative.So cna see themin pics given here.
+
+     */
+    
 }
