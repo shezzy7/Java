8e7dc1054ce3aec6c1c11860eb2745fc62bbbdd3
shezzy7 <jamshahzad72242@gmail.com>
2025-01-16 23:27:43 +0500

    Java

--- Java/33.Graphs_part1/A_graph.java
+++ Java/33.Graphs_part1/A_graph.java
@@ -3,10 +3,20 @@ public class A_graph {
     /*  Graph:-A graph is a network of nodes , in which multiple node are connected tohether with help of edges.Each node in graph is called vertex, and each connecting line connecting a vertex to other is called edge.
      Graph is implemented in many application like google maps etc.
 
-      Types od edges:-
+      Types of edges:-
         Edges can be Uni-directional Or Bi-directional.Uni directional mean we can go from one vertix to other but can't from second to first vertix.In Bi-directional edges we can to both vertices from any vertix.Uni directional are like one way road , while bi-directional are like two way road.We also call bi-directional as un-directed edges.
 
-        Each edge can contain some weight(+ve/-ve).Graphs those edges contain some weight are called weighted graphs and others are called un-weighted graphs.Weight can be positive or negative.So cna see themin pics given here.
+        Each edge can contain some weight(+ve/-ve).Graphs those edges contain some weight are called weighted graphs and others are called un-weighted graphs.Weight can be positive or negative.So can see them in pics given here.
+        Weight is not like kgs etc.It is just a value setted for a specific edge connecting to vertices.
+
+        Representation:-
+          We can represent graphs in many ways.
+          i-Adjacency List:-List of lists
+              In this method we represent  a graph in such a way tha we create a list which contains all the vertices in it.And each element in this list contains a list of all the neighbours connected to it.
+              Basically we store info of edges for each vertix.Starting position of a edge,ending position and weight of the edge.
+          ii-Adjacency Matrix:-
+              
+              
 
      */
     
