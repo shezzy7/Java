4b1c3a30a38c8c770f45109aa7700e917709369b
shezzy7 <jamshahzad72242@gmail.com>
2025-01-19 19:09:54 +0500

    Java

--- Java/33.Graphs_part1/A_graph.java
+++ Java/33.Graphs_part1/A_graph.java
@@ -55,7 +55,36 @@ public class A_graph {
                  This presentation of graph is helpfull for sorting and some specific conditions else this is not advisible.
 
               iv-Implicit Graph
+
                
+
+
+
+
+
+
+              Graph Implementation:-
+                                    a-Maps:In real word scenarios graphs are used in MAPs.We use them for determining difference between any two locations or the strategy used by google maps to find shortest path between any two poits.
+
+                                    b-Social Network:Graphs are used in social network.Like in snapchat,insta,facebook,twitter etc.We can take each user as a vertex and connection between them as an edge.
+
+                                    c-Deleivery Network:Graphs help us in setting deleivery networks.Like if we deleivery truck want to deleiver some material from one point to many other points and after deleivering ,truck wants to come back to origin.For this purpose graph can help in setting a path which will be a path with minimum distance and most good one.
+
+                                    d-Physics & Chemistry: In these fields graphs are helpful when we want to describe them digitally.Like a item making a connection with other item.Then here item can be taken as a vertex and connection between vertices can be taken as edges.
+
+                                    e-Routing algorithms:Internet is a network of connections.Data packets are travelled through routers and reach to different sites of the world.So in this case the routers export data packets from one place to other using routing algorithms.And these algorithms are implemented using graphs.
+
+                                    f-Machine Learning(computation graph): In the fields of deep learning , neural networks are developed using graphs.
+
+                                    g-Dependency Graph:We use graphs for making dependency graphs.A dependecy graph is a structure in which we visualize , which thing is depending upon other one.For example if we want to write documentation for some code and we want to use a built in extension.Then we see that this extension is only useable in vscode.So first we have to download vscode.Then we see that for using vscode we must have windows operating system.So all these dependencies can be structured in a dependency graph using graph.
+
+
+
+
+
+
+                                 
+
 
 
 
