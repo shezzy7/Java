b01368f227c004985d7c4d3c9e451b262bab3543
shezzy7 <jamshahzad72242@gmail.com>
2025-01-20 18:25:05 +0500

    Java

---
+++ Java/33.Graphs_part1/D_createGraph.java
@@ -0,0 +1,7 @@
+public class D_createGraph {
+    
+    public static void main(String args[]){
+        // Here we will create a graph by using adjacency list(List of lists).
+        // For doing so we will creat an array of arraylists.Each arraylist will contain all the newtwork of a vertix.For example if a vertex is making some connections to other edges then ,we will store store info
+    }
+}
