eb3b10e8b2eae9fec2d866fb8aced440c1ff76eb
Shahzad Hussain <shahzadhussain72242@gmail.com>
2025-07-02 16:27:31 +0500

    Delete 21.Linked_List(Part02) directory

--- 21.Linked_List(Part02)/G_doublyLinkedList.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-public class G_doublyLinkedList{
-
-    public static void main(String args[]){
-        //Doubly Linked List:- A list in which each node contians addresses of two nodes,its previous node and its next node.
-        
-    }
-}
