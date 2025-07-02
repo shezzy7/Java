191f8f1ee5ddc1fd4b2ded45407009672a5fafc3
Shahzad Hussain <shahzadhussain72242@gmail.com>
2025-07-02 16:28:00 +0500

    Delete 25.Binary_Trees(Part1) directory

--- 25.Binary_Trees(Part1)/A_BinaryTrees.java
+++
@@ -1,7 +0,0 @@
-
-public class A_BinaryTrees{
-
-    //Tree is hierarchial data structure.In this data strucutre elements are stored like a family chain.
-    //Binary Tree is a specific type of tree in which each node can have  maximum 2 child nodes(1 left child and 1 right child).
-    //Nodes present at the bottom of a parent node are called leaf nodes.
-}
