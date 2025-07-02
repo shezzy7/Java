4a55df500fc093badfc4b6127869c7ac27c73d6d
Shahzad Hussain <shahzadhussain72242@gmail.com>
2025-07-02 16:26:14 +0500

    Delete 14.OOP directory

--- 14.OOP/impThings.java
+++
@@ -1,5 +0,0 @@
-
-//Note:In java we declare classes as public,default but we can't make any class private or protected
-//And we can make our constructors,attributes,fields and sub-classes as default,public,protected or private.
-//If we have created a child class of a parent class.And we want to create object of child class using reference of parent class then we can do this for example in main function if i create object  as->  Parent c = new Child();  . Then it's valid but we can't create object of parent class using reference of child class like         -> Child p = new Parent(); so its unvalid.
-//If we have declared any static type variable in our class then we can also access this variable using class name. 
--- 14.OOP/p_multiple_inheritance.java
+++
@@ -1,3 +0,0 @@
-//Multiple inheritance refers to the concept when there is child class having more then one parent classes.
-//For example ther is class Animal, another class Reptiles  and another class which is derived from these two classes for example we name it Insect then this type of inheritance in Multiple inheritance 
-//But in java multipe inheritance is not allowed
