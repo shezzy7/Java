b9723ccac438f6c632123457c360d59adcb5c94b
Shahzad Hussain <shahzadhussain72242@gmail.com>
2025-07-02 16:25:55 +0500

    Delete 12.Strings directory

--- 12.Strings/length.java
+++
@@ -1,8 +0,0 @@
-public class length{
-    public static void main(String args[]){
-        String name = "Shahzad Hussain";
-        //for getting length of a string in java we use following method 
-        //  stringName.length();
-        System.out.println("Length of your name is : "+name.length());
-    }
-}
