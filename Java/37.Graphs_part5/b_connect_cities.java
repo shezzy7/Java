df3f81b039c0bb0b6495cd1dc6794ea8291708c9
shezzy7 <jamshahzad72242@gmail.com>
2025-09-03 17:04:32 +0500

    Java

--- Java/37.Graphs_part5/b_connect_cities.java
+++ Java/37.Graphs_part5/b_connect_cities.java
@@ -1,3 +1,62 @@
+import java.util.*;
 public class b_connect_cities {
     
+    static class Edge implements Comparable<Edge>{
+        int dst , wt;
+        public Edge(int d , int w){
+
+            this.dst = d;
+            this.wt = w;
+
+        }
+        @Override
+        public int compareTo(Edge e){
+            return this.wt-e.wt;
+        }
+        
+    }
+
+    public static int connectCities(int cities[][] , int n){
+        // Find the minimum cost for connecting all the cities on the map.
+        // here in our 2d array arr[i][j] means , is there any edge connectin ith vertex with jth vertex. If value at that index is not 0 then its mean there is an edge between i and j with weight of arr[i][j].If arr[i][j] is 0 then its mean there is no edge between i and j.
+        // Keeping this thing in mind , lets create an adjacency list.
+        int cost = 0;
+        PriorityQueue<Edge>q = new PriorityQueue<>();
+        boolean visited[] = new boolean[n];
+        q.add(new Edge(0, 0));
+
+        while(!q.isEmpty()){
+            Edge e = q.remove();
+            if(!visited[e.dst]){
+                visited[e.dst] = true;
+                cost+=e.wt;
+
+                // now add neighbours of first vertex
+                for(int i=0;i<cities[e.dst].length;i++){
+                    if(cities[e.dst][i]!=0){
+
+                        q.add(new Edge(i,cities[e.dst][i]));
+                    }
+                }
+
+
+            }
+
+        }
+
+         
+        return cost;
+    }
+    public static void main(String args[]){
+        int cities[][] = {
+            {0,1,2,3,4},
+            {1,0,5,0,7},
+            {2,5,0,6,0},
+            {3,0,6,0,0},
+            {4,7,0,0,0}
+        };
+
+        int ans = connectCities(cities , cities.length);
+        System.out.print(ans);
+    }
 }
--- Java/40.DP_Part2/d_01_knapsack_tabulations.java
+++ Java/40.DP_Part2/d_01_knapsack_tabulations.java
@@ -1,27 +1,47 @@
 public class d_01_knapsack_tabulations {
     
     public static int knapsack(int val[] , int wt[] , int W , int n){
+
+        // No we are going to solve this problem with the help of tabulation concept , We will be doing same thing as we have done in memoization. But we will be doing this with the help of iteration instead of recursion.
+        // 
         int dp[][] = new int[n+1][W+1];
-        for(int i=0;i<n+1;i++){
-            for(int j=0;j<W+1;j++){
-                dp[i][j] = -1;
-            }
+
+        for(int i=0;i<n+1;i++){ //1st col all rows filled with 0s.
+            dp[i][0]=0;
+
         }
-        for(int i=0;i<n+1;i++){
-            for(int j=0;j<W+1;j++){
-                if(i==0 || j==0){
-                    dp[i][j] = 0;
+        for(int i=0;i<W+1;i++){ //1st row all cols filled with 0s
+            dp[0][i]=0;
+        }
+ 
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<W+1;j++){
+                // here in dp by i we mean number of items that we are going to use at this time and by j we mean weight of knapsack that we are dealing currently.
+                // if we want to get value of first item then we will do this by i-1 , as we are running our loop by 1 instead of 0 due to traversing over dp array , so if we write i-1 then it will become 0 which is starting index. Also we can get weight of ith item by i-1
+                int curr_val = val[i-1];
+                int curr_w = wt[i-1];
+
+                // if weight of current item is less then current knapsack capacity then we can add this in our dp array 
+                if(curr_w<=j){
+                    //And also at that point we have two choices whather to add or not this item in our current result. For this we will see if adding this gives us more profit or excluding this gives more.
+
+
+                    
+                    dp[i][j] = Math.max(curr_val + dp[i-1][j-curr_w], dp[i-1][j]);
                 }
-                else if(wt[i-1]<=j){
-                    dp[i][j] = Math.max(val[i-1] + dp[i-1][j-wt[i-1]], dp[i-1][j]);
-                }
+
                 else{
+                    // else if capacity of knapsack is less then current items weight then we need to exclude this and for this situation we can take answer of previous situation. 
                     dp[i][j] = dp[i-1][j];
                 }
+
             }
         }
-        return dp[n][W];
+
+        return dp[n][W]; //at last index our final value will be present there.
+
     }
+
     public static void main(String args[]){
         int val[] = {15 , 14 , 10 , 45 , 30};
         int wt[] = {2 , 5 , 1 , 3 , 4};
